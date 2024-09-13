f7c97be26c04549b73b903393c59fa234971b871
Abhishek Choudhary <abhishek.choudhary7631@gmail.com>
2024-09-13 08:10:04 +0530

    code modified

--- script.js
+++ script.js
@@ -197,13 +197,3 @@ nextButton.addEventListener("click", () => {
 });
 
 quiz();
-
-nextButton.addEventListener("click", () => {
-  if (currQuestionIndex < question.length) {
-    handleNextButton();
-  } else {
-    quiz();
-  }
-});
-
-quiz();
